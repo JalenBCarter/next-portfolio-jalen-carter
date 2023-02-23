fee032098b9a0327ee2cb7bc5e22137037011c0d
Jalen Carter <neuralgroovejbc@gmail.com>
2023-02-22 21:36:39 -0400

    added navbar

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 
+// importing components
+import Navbar from './components/Navbar'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -15,7 +18,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        
+        <Navbar/>
       </main>
     </>
   )
