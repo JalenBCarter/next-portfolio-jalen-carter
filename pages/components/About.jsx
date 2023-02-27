cd6d19e54ceedd9fabab88c9c9e34d099c2503b3
Jalen Carter <neuralgroovejbc@gmail.com>
2023-02-27 14:16:32 -0400

    About component added

---
+++ pages/components/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div className="container mx-auto flex justify-center">
+            <div className="p-5 my-5 md:mx-[100px] text-justify">
+                <div className="text-xl">What I'm all about</div>
+                <div className="border border-gray-300 my-3"></div>
+                <p>
+                My goal as a junior developer is to leverage my skills to create impactful digital experiences for my clients. With a solid understanding of programming languages and a passion for innovation, I am eager to collaborate with others to design and develop high-quality software solutions that meet client requirements. I am dedicated to staying current with emerging technologies to deliver cutting-edge solutions that exceed client expectations.
+                </p>
+            </div>
+        </div>
+    )
+}
