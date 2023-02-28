6124952a97ca5ec7331014ff2e1b682dfd1aa8db
Jalen Carter <neuralgroovejbc@gmail.com>
2023-02-28 18:10:15 -0400

    added hero

--- pages/components/Hero.jsx
+++ pages/components/Hero.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import Image from 'next/image'
-import mtns from '../../public/images/bg-image.jpg'
+import hero from '../../public/images/hero.jpg'
 // import {Dimensions} from 'react-native';
 
 
 export const Hero = () => {
   return (
     <div className=''>
-      <Image
-        src={mtns}
+      {/* <Image
+        src={hero}
         alt='Mountains'
         className={'heroImage'}
-      />
+      /> */}
+      <div className="heroBackground"></div>
       <div className="flex justify-center items-center">
         {/* <h1 className="heroText">Hi, I'm Jalen</h1> */}
         {/* <h3 className="font-weight-light">Web Programmer</h3> */}
