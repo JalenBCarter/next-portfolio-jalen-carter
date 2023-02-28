74a9b82ab033f32969ac7d77d9eb30813715b9b9
Jalen Carter <neuralgroovejbc@gmail.com>
2023-02-28 15:51:49 -0400

    about layout done

--- pages/components/About.jsx
+++ pages/components/About.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 
 export const About = () => {
     return (
-        <div className="container mx-auto flex flex-col items-center my-[100px]">
+        <div className="px-[15px] sm:px-[50px] lg:px-[50px] mx-auto flex flex-col lg:flex-row items-center justify-around my-[100px] ">
             <div className='headshot my-5'></div>
 
-            <div className="text-justify">
-                <div className="text-xl">What I'm all about</div>
-                <div className="border border-gray-300 my-3"></div>
-                <div>
-                    My goal as a junior developer is to leverage my skills to create impactful digital experiences for my clients. With a solid understanding of programming languages and a passion for innovation, I am eager to collaborate with others to design and develop high-quality software solutions that meet client requirements. I am dedicated to staying current with emerging technologies to deliver cutting-edge solutions that exceed expectations.
+            <div className="lg:w-[70%]">
+                <div className="text-xl lg:w-[50%]">What I'm all about</div>
+                <div className="border border-gray-500 my-3"></div>
+                <div className='text-justify text-gray-500'>
+                    My goal as a junior developer is to leverage my skills to create impactful digital experiences 
+                    for my clients. With a solid understanding of programming languages and a passion for innovation, 
+                    I am eager to collaborate with others to design and develop high-quality software solutions that meet 
+                    client requirements. I am dedicated to staying current with emerging technologies to deliver cutting-edge 
+                    solutions that exceed expectations.
                 </div>
             </div>
         </div>
--- pages/components/Portfolio.jsx
+++ pages/components/Portfolio.jsx
@@ -8,22 +8,22 @@ export const Portfolio = () => {
         {
             "name": "Ski App",
             "description": "Hello World",
-            "image" : "/skiappcrud.png"
+            "image": "/skiappcrud.png"
         },
         {
             "name": "Launchpad App",
             "description": "Hello World",
-            "image" : "/launchpadadmin.png"
+            "image": "/launchpadadmin.png"
         },
         {
             "name": "Ski App",
             "description": "Hello World",
-            "image" : "/techroster.png"
+            "image": "/techroster.png"
         },
         {
             "name": "Ski App",
             "description": "Hello World",
-            "image" : "/spacetagram.png"
+            "image": "/spacetagram.png"
         },
     ]
 
@@ -31,9 +31,9 @@ export const Portfolio = () => {
         <div className="p-4 mt-[40px] w-[100%] flex flex-col items-center">
             <div className="text-3xl">Portfolio</div>
             <div className="border border-gray-300 my-3 w-[100%]"></div>
-            <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-4 w-[100%]">
                 {projects.map((proj, key) =>
-                    <div key={key} class="bg-white rounded-lg shadow m-2 ">
+                    <div key={key} class="rounded-lg shadow-md m-2 hover:shadow-lg">
                         {/* <a href="#">
                             <Image
                                 src={mtns}
@@ -41,13 +41,14 @@ export const Portfolio = () => {
                                 className="rounded-t-lg"
                             />
                         </a> */}
-                        <div className="w-[100%] h-[300px] rounded-t-lg" style={{backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.3)),
-    url(${proj.image})`, backgroundSize: "cover"}} ></div>
+                        <div className="w-[100%] h-[300px] rounded-t-lg" style={{
+                            backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.3)), url(${proj.image})`, backgroundSize: "cover"
+                        }} ></div>
                         <div class="p-5">
                             <a href="#">
-                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{proj.name}</h5>
                             </a>
-                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                            <p className="mb-3 font-normal text-gray-700">{proj.description}</p>
                             <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-200 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 Read more
                                 <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
